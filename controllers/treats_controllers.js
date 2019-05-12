90764e78b2c85f7e0cb96e32b53da3cbb66cb8c5
Mosbycr <mosbycr@gmail.com>
2019-05-12 14:50:49 -0400

    server.js set up and connecting, treats controllers export set up

---
+++ controllers/treats_controllers.js
@@ -0,0 +1,9 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/treats.js");
+
+
+module.exports = router;
