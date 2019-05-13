b09d4b8191923c8767ed5bf9acae5076788087b3
Mosbycr <mosbycr@gmail.com>
2019-05-12 20:59:00 -0400

    added orm all in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,14 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-    
-}
+        all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+        if (err) {
+            throw err;
+        }
+        cb(result);
+        });
+    }
+};
 module.exports = orm;
