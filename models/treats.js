107ababb949248ce349dfa0525aeeca7dbba9937
Mosbycr <mosbycr@gmail.com>
2019-05-12 21:28:21 -0400

    added treats.js selectAll in treats variable

--- models/treats.js
+++ models/treats.js
@@ -1,6 +1,10 @@
 var orm = require("../config/orm.js");
 
 var treats = {
-    
-}
+  selectAll: function(cb) {
+    orm.all("treats", function(res) {
+      cb(res);
+    });
+  }
+};
 module.exports = treats;
