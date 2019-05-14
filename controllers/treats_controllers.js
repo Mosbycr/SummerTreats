a52a0cc838e126f8c490719e8e35bbd58098b840
Mosbycr <mosbycr@gmail.com>
2019-05-13 20:26:59 -0400

    added router.updateOne to treatsController.js

--- controllers/treats_controllers.js
+++ controllers/treats_controllers.js
@@ -14,4 +14,25 @@ router.get("/", function(req, res) {
   });
 });
 
+router.put("/api/treats/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("current id: " + condition);
+
+  treats.updateOne(
+    {
+      devoured: req.body.devoured //not sure if this is correct yet
+    },
+    condition,
+    function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
 module.exports = router;
