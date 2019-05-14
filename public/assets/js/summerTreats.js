c108d9e0071088ff454739af125dcb4f10af0097
Mosbycr <mosbycr@gmail.com>
2019-05-13 20:43:13 -0400

    added treat input submit on click adding new treat name post to database

--- public/assets/js/summerTreats.js
+++ public/assets/js/summerTreats.js
@@ -15,4 +15,23 @@ $(function() {
       location.reload();
     });
   });
+
+  $("#treatSumbit").on("click", function(event) {
+    event.preventDefault();
+
+    var newTreat = {
+      treat_name: $("#treatName")
+        .val()
+        .trim()
+    };
+
+    $.ajax("/api/treats", {
+      type: "POST",
+      data: newTreat
+    }).then(function() {
+      console.log("added a new treat");
+
+      location.reload();
+    });
+  });
 });
