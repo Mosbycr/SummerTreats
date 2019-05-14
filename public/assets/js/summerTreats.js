6538eb39de00d26249e56be65f22aaaa78df99d8
Mosbycr <mosbycr@gmail.com>
2019-05-13 20:11:35 -0400

    added summerTreats on click function when hit devour button change devour to true

---
+++ public/assets/js/summerTreats.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("#devbtn").on("click", function(event) {
+    var id = $(this).data("id");
+    // var nowEaten = $(this).data("nowEaten");
+
+    var nowEaten = {
+      devoured: true
+    };
+
+    $.ajax("/api/treats/" + id, {
+      type: "PUT",
+      data: nowEaten
+    }).then(function() {
+      console.log("changed devoured to TRUE");
+      location.reload();
+    });
+  });
+});
