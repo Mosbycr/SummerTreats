f59ea23749a9807ba8612ca06f1517d6b96b5e15
Mosbycr <mosbycr@gmail.com>
2019-05-13 21:24:02 -0400

    added comments to treatscontrollers and moves post back below put as it is in other files

--- controllers/treats_controllers.js
+++ controllers/treats_controllers.js
@@ -1,9 +1,10 @@
 var express = require("express");
 
 var router = express.Router();
-
+//grab treats.js
 var treats = require("../models/treats.js");
 
+//route collects all information in treats table and places it where stated in index.handlebars
 router.get("/", function(req, res) {
   treats.selectAll(function(data) {
     var hbsObject = {
@@ -14,24 +15,15 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/treats", function(req, res) {
-  treats.insertOne(
-    ["treat_name"],
-    [req.body.treat_name],
-    function(result) {
-      res.json({ id: result.insertID });
-    }
-  );
-});
-
+// route that grabs the id of the treat devoured and updates devoured col to true
 router.put("/api/treats/:id", function(req, res) {
   var condition = "id = " + req.params.id;
+  //console.log("current id: " + condition);
 
-  console.log("current id: " + condition);
-
+  // connected to devoured: true in summerTreats.js
   treats.updateOne(
     {
-      devoured: req.body.devoured // connected to devoured: true in summerTreats.js
+      devoured: req.body.devoured 
     },
     condition,
     function(result) {
@@ -44,6 +36,11 @@ router.put("/api/treats/:id", function(req, res) {
   );
 });
 
-
+//takes information from textbox and creates new treat in database with devoured default false
+router.post("/api/treats", function(req, res) {
+  treats.insertOne(["treat_name"], [req.body.treat_name], function(result) {
+    res.json({ id: result.insertID });
+  });
+});
 
 module.exports = router;
