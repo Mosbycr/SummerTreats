4bd00f4132494fedcd283bcd0b914fb93a667b39
Mosbycr <mosbycr@gmail.com>
2019-05-13 20:46:15 -0400

    added insertOne function to orm

--- config/orm.js
+++ config/orm.js
@@ -57,6 +57,26 @@ var orm = {
 
       cb(result);
     });
+  },
+  insertOne: function(table, cols, vals, cb){
+       var queryString = "INSERT INTO " + table;
+
+       queryString += " (";
+       queryString += cols.toString();
+       queryString += ") ";
+       queryString += "VALUES (";
+       queryString += printQuestionMarks(vals.length);
+       queryString += ") ";
+
+       console.log(queryString);
+
+       connection.query(queryString, vals, function(err, result) {
+         if (err) {
+           throw err;
+         }
+
+         cb(result);
+       });
   }
 };
 module.exports = orm;
