f7097d82d5797201779a3eb450f4e598552dac47
Mosbycr <mosbycr@gmail.com>
2019-05-13 20:20:38 -0400

    added orm for update in orm js

--- config/orm.js
+++ config/orm.js
@@ -1,45 +1,62 @@
 var connection = require("../config/connection.js");
 
-// function printQuestionMarks(num) {
-//   var arr = [];
+function printQuestionMarks(num) {
+  var arr = [];
 
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
 
-//   return arr.toString();
-// }
+  return arr.toString();
+}
 
-// // Helper function to convert object key/value pairs to SQL syntax
-// function objToSql(ob) {
-//   var arr = [];
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
 
-//   // loop through the keys and push the key/value as a string int arr
-//   for (var key in ob) {
-//     var value = ob[key];
-//     // check to skip hidden properties
-//     if (Object.hasOwnProperty.call(ob, key)) {
-//       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//         value = "'" + value + "'";
-//       }
-//       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-//       // e.g. {sleepy: true} => ["sleepy=true"]
-//       arr.push(key + "=" + value);
-//     }
-//   }
-//   return arr.toString();
-// }
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
 
 var orm = {
-        selectAll: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, result) {
-        if (err) {
-            throw err;
-        }
-        cb(result);
-        });
-    }
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  updateOne: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
 };
 module.exports = orm;
